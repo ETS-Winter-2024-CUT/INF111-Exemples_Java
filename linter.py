bb421c4eac19fb9f455e4b80311236164e7b92c3
Vianpyro <vianney.veremme@gmail.com>
2023-12-22 15:07:48 -0500

    Ajout linter et Workflow

---
+++ linter.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+"""
+Ce script est concu pour effectuer des verifications specifiques dans les \
+    fichiers de code, telles que la longueur des lignes et la structure des \
+    en-tetes de fichier.
+"""
+import glob
+import os
+import re
+import sys
+
+# Workflow settings
+EXIT_STATUS_SUCCESS = 0
+EXIT_STATUS_FAILURE = 1
+
+# Default settings
+DEFAULT_RULER = 80
+
+
+def print_error(line_num, message) -> None:
+    print(f"\tLigne {line_num + 1:>3}: {message}")
+
+
+def verify_ruler(lines: list[str], ruler: int) -> bool:
+    success_status = EXIT_STATUS_SUCCESS
+
+    for index, line in enumerate(lines):
+        line_lenght = len(line.strip())
+
+        if line_lenght > ruler:
+            success_status = EXIT_STATUS_FAILURE
+            print_error(index, f"Ligne trop longue ({line_lenght} > {ruler}).")
+
+    return success_status
+
+
+def verify_header_comment(lines: list[str]):
+    # Find the index where the imports end
+    import_end_index = len(lines)
+
+    for i, line in enumerate(lines):
+        if not line.strip().startswith("import "):
+            import_end_index = i
+            break
+
+    # Combine lines after imports to search for header comment
+    rest_of_code = "".join(lines[import_end_index:])
+
+    # Define the regex pattern to match the header comment
+    pattern = r"/\*.*?\*/"
+    header_comment = re.search(pattern, rest_of_code, re.DOTALL)
+
+    return EXIT_STATUS_SUCCESS if header_comment else EXIT_STATUS_FAILURE
+
+
+def main(files: list[list]) -> bool:
+    exit_status = EXIT_STATUS_SUCCESS
+
+    for filename in files:
+        if not os.path.exists(filename):
+            print(f"Fichier introuvable : {filename}")
+            raise FileNotFoundError
+
+        # Ouverture et traitement du fichier
+        print(f'Ouverture de "{filename}":')
+        with open(filename, "r") as f:
+            lines = f.readlines()
+
+            if verify_header_comment(lines) == EXIT_STATUS_FAILURE:
+                exit_status = EXIT_STATUS_FAILURE
+
+            if verify_ruler(lines, DEFAULT_RULER) == EXIT_STATUS_FAILURE:
+                exit_status = EXIT_STATUS_FAILURE
+
+        print(f'Fermeture de "{filename}".\n')
+
+    return exit_status
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        script_name = os.path.splitext(os.path.basename(__file__))[0]
+        print(f"Utilisation: python {script_name}.py file(s)")
+        exit(EXIT_STATUS_FAILURE)
+
+    file_patterns = sys.argv[1:]
+    files = []
+
+    for pattern in file_patterns:
+        # Utilisation de glob pour trouver les fichiers correspondants au motif
+        matching_files = glob.glob(pattern)
+        files.extend(matching_files)
+
+    if not files:
+        print("Aucun fichier correspondant trouve.")
+        exit(EXIT_STATUS_FAILURE)
+
+    exit_status = main(files)
+    exit(exit_status)
