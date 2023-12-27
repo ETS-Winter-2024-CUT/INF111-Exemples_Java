17b2c50bc7835e443f814954ed97cfce2ba11b31
Vianpyro <10519369+Vianpyro@users.noreply.github.com>
2023-12-26 21:14:02 -0500

    Respect de la limite de 100 caractères

--- src/ValiderEntreBornesConstantes.java
+++ src/ValiderEntreBornesConstantes.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 /*
     Valider qu'un nombre est compris entre 1 et 100.
-    Pour ce faire, définire deux constantes avec les valeurs 1 et 100 et une variable de type entier.
+    Pour ce faire, définire deux constantes avec les valeurs 1 et 100 et une variable de type entier
     Dans une boucle, saisir une valeur au clavier, vérifier qu'elle est entre 1 et 100,
         si elle ne l'est pas, afficher un message d'erreur et redemander a nouveau la valeur.
     La boucle se termine lorsque le nombre est valide.
@@ -19,16 +19,17 @@ public class ValiderEntreBornesConstantes {
         int valeur;
 
         do {
-            System.out.println("Entrez une valeur comprise entre " + VALEUR_MIN + " et " + VALEUR_MAX + " (inclus) : ");
+            System.out.println("Entrez une valeur comprise entre " + VALEUR_MIN + " et " +
+                    VALEUR_MAX + " (inclus) : ");
             valeur = scanner.nextInt();
 
             if (valeur < VALEUR_MIN || valeur > VALEUR_MAX) {
-                System.out.println(
-                        "Erreur, " + valeur + " n'est pas compris entre " + VALEUR_MIN + " et " + VALEUR_MAX + "!");
+                System.out.println("Erreur, " + valeur + " n'est pas compris entre " + VALEUR_MIN +
+                        " et " + VALEUR_MAX + "!");
             }
         } while (valeur < VALEUR_MIN || valeur > VALEUR_MAX);
 
-        System.out.println(valeur); 
+        System.out.println(valeur);
         scanner.close();
     }
 }
