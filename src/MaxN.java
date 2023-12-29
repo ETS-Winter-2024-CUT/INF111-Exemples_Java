0cc4652e8311d8a9a8ce9728c4319f7360a6f19d
Vianpyro <10519369+Vianpyro@users.noreply.github.com>
2023-12-29 12:25:43 -0500

    Ajout de commentaires

--- src/MaxN.java
+++ src/MaxN.java
@@ -3,42 +3,45 @@ import java.util.Scanner;
 public class MaxN {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        // Demande à l'utilisateur le nombre de valeurs à comparer
         int nombre_valeurs = saisir_entier_positif(scanner);
+        // Trouve la valeur maximale parmi les valeurs saisies
         double valeur_max = trouver_max(scanner, nombre_valeurs);
 
-        System.out.printf("La plus grande valeur est: ");
-        if ((int) valeur_max == valeur_max) {
-            System.out.println((int) valeur_max);
-        } else {
-            System.out.println(valeur_max);
-        }
+        // Affiche la plus grande valeur saisie
+        System.out.printf("La plus grande valeur est: %.2f", valeur_max);
 
-        scanner.close();
+        scanner.close(); // Ferme le Scanner pour libérer les ressources
     }
 
+    // Méthode pour saisir un entier positif supérieur ou égal à 2
     private static int saisir_entier_positif(Scanner scanner) {
         int valeur;
 
+        // Vérifie que le nombre est supérieur à 2, redemander tant que nécessaire
         do {
-            System.out.println("Entrez le nombre de valeurs a comparer:");
+            System.out.println("Entrez le nombre de valeurs à comparer:");
             valeur = scanner.nextInt();
         } while (valeur < 2);
 
         return valeur;
     }
 
+    // Méthode pour trouver la valeur maximale parmi les valeurs saisies
     private static double trouver_max(Scanner scanner, int nombre_valeurs) {
         System.out.printf("Entrez %d valeurs:\n", nombre_valeurs);
-
-        double max = Double.NEGATIVE_INFINITY;
+        
+        // Initialise la variable max avec une valeur minimale possible
+        double max = Double.NEGATIVE_INFINITY; 
 
         for (int i = 0; i < nombre_valeurs; i++) {
-            double nouvelle_valeur = scanner.nextDouble();
+            double nouvelle_valeur = scanner.nextDouble(); // Saisie de la nouvelle valeur
 
+            // Mise à jour de la valeur maximale si la nouvelle valeur est plus grande
             if (nouvelle_valeur > max)
-                max = nouvelle_valeur;
+                max = nouvelle_valeur; 
         }
 
-        return max;
+        return max; // Retourne la valeur maximale parmi celles saisies
     }
 }
