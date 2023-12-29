035716f2532d79fe5e5f2305f6510ae85c0dbec4
Vianpyro <10519369+Vianpyro@users.noreply.github.com>
2023-12-29 14:34:50 -0500

    Utilisation de Javadocs

--- src/NombreJoursADate.java
+++ src/NombreJoursADate.java
@@ -1,6 +1,15 @@
 import java.util.Scanner;
 
+/**
+ * Programme pour déterminer le nombre de jours dans un mois pour une année donnée.
+ */
 public class NombreJoursADate {
+
+    /**
+     * Méthode principale pour exécuter le programme.
+     *
+     * @param args Arguments de la ligne de commande (non utilisés dans ce programme)
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -27,7 +36,13 @@ public class NombreJoursADate {
         scanner.close(); // Fermeture du scanner pour libérer les ressources
     }
 
-    // Méthode pour déterminer le nombre de jours dans un mois donné pour une année donnée
+    /**
+     * Méthode pour déterminer le nombre de jours dans un mois donné pour une année donnée.
+     *
+     * @param annee Année pour laquelle le nombre de jours est calculé
+     * @param mois  Mois pour lequel le nombre de jours est calculé
+     * @return      Le nombre de jours dans le mois pour l'année donnée
+     */
     private static int nbr_jours_mois(int annee, int mois) {
         switch (mois) {
             case 1:
@@ -51,7 +66,12 @@ public class NombreJoursADate {
         }
     }
 
-    // Méthode pour vérifier si une année est bissextile ou non
+    /**
+     * Méthode pour vérifier si une année est bissextile ou non.
+     *
+     * @param annee Année à vérifier pour la bissextile
+     * @return      True si l'année est bissextile, sinon False
+     */
     private static boolean annee_est_bisextile(int annee) {
         // Vérification des conditions pour qu'une année soit bissextile
         return (annee % 4 == 0 && annee % 100 != 0) || (annee % 400 == 0);
