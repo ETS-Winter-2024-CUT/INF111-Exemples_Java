66ae5dcdd380707ed4bdf0af0d8da8e57d5c5953
Vianpyro <10519369+Vianpyro@users.noreply.github.com>
2023-12-29 16:31:10 -0500

    Création de la classe ManipulationListeEntiers

---
+++ src/ManipulationListeEntiers.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Cette classe permet à l'utilisateur de manipuler une liste d'entiers en effectuant diverses 
+ *  opérations telles que l'ajout, l'affichage, le calcul de la somme, la recherche du plus grand 
+ *  élément et la suppression d'un élément spécifique de la liste.
+ */
+
+public class ManipulationListeEntiers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Création d'une liste pour stocker des entiers
+        List<Integer> liste_entiers = new ArrayList<>();
+
+        System.out.println("Combien d'elements souhaitez-vous ajouter a la liste?");
+        int nombre_elements = scanner.nextInt();
+
+        System.out.println("Veuillez saisir les elements:");
+        for (int i = 0; i < nombre_elements; i++) {
+            int element = scanner.nextInt(); // Demande à l'utilisateur de saisir chaque élément
+            liste_entiers.add(element); // Ajout de l'élément à la liste
+        }
+
+        // Affichage de la taille de la liste
+        System.out.printf("Il y'a %d elements dans la liste\n", liste_entiers.size());
+
+        // Affichage des éléments de la liste
+        System.out.println("Contenu de la liste: " + liste_entiers);
+
+        // Calcul et affichage de la somme des éléments de la liste
+        int somme = 0;
+
+        for (int element : liste_entiers) {
+            somme += element; // Ajoute chaque élément à la somme
+        }
+
+        System.out.println("La somme des elements est " + somme);
+
+        // Affichage du plus grand élément de la liste
+        int maximum = Collections.max(liste_entiers); // Trouve le plus grand élément dans la liste
+        System.out.println("Le plus grand element est " + maximum);
+
+        // Suppression d'un élément de la liste en fonction de l'indice fourni par l'utilisateur
+        System.out.println("Entrez l'indice de l'element a supprimer");
+        int index_element_a_supprimer = scanner.nextInt();
+        liste_entiers.remove(index_element_a_supprimer); // Supprime l'élément à l'indice spécifié
+        System.out.println("Liste mise a jour: " + liste_entiers);
+
+        scanner.close();
+    }
+}
