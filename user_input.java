180c1bf43f8e8918638645ee164ececd40c61a7a
Leonard Marcoux <140647944+AdelnorAcmorux@users.noreply.github.com>
2024-01-05 10:11:04 -0500

    Update user_input.java
    Co-authored-by: Vianney Veremme <10519369+Vianpyro@users.noreply.github.com>

--- user_input.java
+++ user_input.java
@@ -30,6 +30,6 @@ public class user_input {
 
         System.out.println("Hello " + name);
         System.out.println("You are "+age+" years old");
-        System.out.println("Your favorite food is "+food);
+        System.out.println("Your favorite food is " + food);
     }
 }
